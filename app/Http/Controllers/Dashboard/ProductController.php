5d7c10296b9ae885a87b8a28cf023e432d1a3ae1
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-09-24 19:01:22 +0300

    Make a Global Scope

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -5,21 +5,15 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
     public function getAllProducts(Request $request)
     {
         $filters = $request->query();
-    
         $products = Product::filter($filters)->paginate();
-        // leftJoin('categories as parents', 'parents.id', '=', 'categories.parent_id')
-        //     ->select(['categories.*', 'parents.name as parent_name'])
-        //     ->filter($filters)
-        //     ->paginate();
 
-        // return view('product', compact('products'));
-    
         return response()->json(['data' => $products]);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\StoreScope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Support\Facades\Auth;
 
 class Product extends Model
 {
@@ -26,6 +29,27 @@ class Product extends Model
         'status',
     ];
 
+    // static function booted as a bootsrab in service container 
+    // as a constructor 
+    // worked first when use this odel 
+    // protected static function booted()
+    // {
+    //     // in controller can i use withoutGlobalScope
+    //     static::addGlobalScope('store', function (Builder $builder) {
+    //         $user = Auth::user();
+            
+    //         if ($user->store_id) {
+    //             $builder->where('store_id', '=', $user->store_id);
+    //         }
+    //     });
+    // }
+    protected static function booted()
+    {
+        // that is error    "message": "Global scope must be an instance of Closure or Scope.",
+        // he said i want a object from class not class 
+        // static::addGlobalScope('store', StoreScope::class);
+        static::addGlobalScope('store' , new StoreScope());
+    }
     public function scopeFilter(EloquentBuilder $builder, $filters)
     {
         $name = $filters['name'] ?? null;
