bebe3b3006e120d5df2d3e87166b912a8d3fa9c1
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-10-02 18:35:25 +0300

    use getCookieId in all of method

--- app/Repositories/Cart/CartModelRepository.php
+++ app/Repositories/Cart/CartModelRepository.php
@@ -14,14 +14,14 @@ class CartModelRepository implements CartRepository
 {
     public function get(): Collection
     {
-        return Cart::where('cookie_id' , '=' , '')->get();
+        return Cart::where('cookie_id' , '=' , $this->getCookieId())->get();
     }
 
     
     public function add(Product $product , $quantity=1)
     {
         return Cart::create([
-            'cookie_id' =>Str::uuid(),
+            'cookie_id' =>$this->getCookieId(),
             'user_id' => Auth::id(),
             'product_id' => $product->id(),
             'quantity' => $quantity,
@@ -30,24 +30,28 @@ class CartModelRepository implements CartRepository
 
     public function update(Product $product, $quantity)
     {
-        Cart::where('product_id', $product->id)->update([
-            'quantity' => $quantity,
-        ]);
+        Cart::where('product_id', '=',$product->id)
+            ->where('cookie_id' ,'=', $this->getCookieId()) 
+            ->update([
+                'quantity' => $quantity,
+            ]);
     }
 
     public function delete(Product $product)
     {
-        Cart::where('product_id', $product->id)->delete();
+        Cart::where('product_id', '=',$product->id)
+            ->where('cookie_id' ,'=', $this->getCookieId()) 
+            ->delete();
     }
 
     public function empty()
     {
-        Cart::where('cookie_id')->delete();
+        Cart::where('cookie_id' ,'=', $this->getCookieId()) ->delete();
     }
 
     public function total() : float
     {
-        return Cart::where('cookie_id' , '=' , '')
+        return Cart::where('cookie_id' , '=' ,  $this->getCookieId())
             ->join('products' , 'product.id', '=' , 'carts.product_id')
             ->selectRow('SUM(products.price * carts.quantity) as total')
             ->value('total');
